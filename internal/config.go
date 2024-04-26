4afa282e149253635e425f2dfb064ceef67d189a
taoning <tnwang@proton.me>
2024-04-26 13:12:17 -0700

    make gpt-4-turbo default model

--- internal/config.go
+++ internal/config.go
@@ -2,22 +2,22 @@ package internal
 
 import (
 	"os"
-    "os/exec"
+	"os/exec"
 
 	"github.com/spf13/viper"
 )
 
 func EditConfig(editor string, filePath string) error {
-    cmd := exec.Command(editor, filePath)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    return cmd.Run()
+	cmd := exec.Command(editor, filePath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
 
 func SetDefaultConfig() {
 	viper.SetDefault("url", "https://api.openai.com/v1/chat/completions")
-	viper.SetDefault("model", "gpt-4-1106-preview")
+	viper.SetDefault("model", "gpt-4-turbo")
 	viper.SetDefault("chat.fileName", ".jchat")
 	viper.SetDefault("chat.systemMessage", "You are an polymath who is an expert is all scientific and engineering fields. You are able to leverage knowledge in other domains to solve the problem at hand. You carefully provide accurate, factual, thoughtful, nuanced answers, and are brilliant at reasoning. If you think there might not be a correct answer, you say so. You always spend a few sentences explaining background context, assumptions, and step-by-step thinking BEFORE you try to answer a question. Dont be verbose in your answers, but do provide details and examples where it might help the explanation. When showing code, minimise vertical space, and do not include comments or docstrings.")
 	viper.SetDefault("editor.fileName", ".jedit")
